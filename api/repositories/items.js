d82c6fd50f5da6d9de0c7c7a36cfee0e2d7ddc25
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-01 23:29:42 +0900

    Update items repo

--- api/repositories/items.js
+++ api/repositories/items.js
@@ -1,7 +1,23 @@
 export default $axios => {
   const all = () => {
-    return []
+    return $axios.get(`/api/items/`)
   }
 
-  return { all }
+  const findOne = (uuid, option) => {
+    return $axios.get(`/api/items/${uuid}`)
+  }
+
+  const create = (uuid, payload) => {
+    return $axios.post(`/api/items/${uuid}`, payload)
+  }
+
+  const patchOne = (uuid, option) => {
+    return $axios.patch(`/api/items/${uuid}`)
+  }
+
+  const destroyOne = (uuid, option) => {
+    return $axios.delete(`/api/items/${uuid}`)
+  }
+
+  return { all, findOne, create, patchOne, destroyOne }
 }
