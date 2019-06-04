b7685f602a5a462440c619a1a016acbe64b0c8ff
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-04 22:31:54 +0900

    Update styles

--- components/Task/NewTaskInput.vue
+++ components/Task/NewTaskInput.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="mb-4">
-    <label for="new-form-input">New Task</label>
+    <label for="new-form-input" class="font-bold text-grey-darker block mb-2">
+      New Task
+    </label>
     <input
       id="new-form-input"
       v-model.trim="title"
       type="text"
-      class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+      class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal font-semibold text-lg"
       required
+      autocomplete="off"
       @keyup.enter="trySubmit"
     />
   </div>
