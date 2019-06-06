906c4df58f411b7324a32704520c9ddf76bea0a3
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-06 09:53:52 +0200

    리스트 추가

--- components/Common/Sidebar.vue
+++ components/Common/Sidebar.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="flex-none w-full md:max-w-xs bg-gray-200">
+  <div id="sidebar" class="flex-none w-full md:max-w-xs bg-gray-200">
     <div class="p-4">
       <button
         class="border border-blue-500 rounded px-4 py-2 text-blue-500 bg-white w-full hover:bg-blue-500 hover:text-white"
+        @click="openNewListForm = true"
       >
         New List
+      </button>
+    </div>
+    <div
+      v-if="openNewListForm"
+      class="m-4 p-4 bg-white shadow"
+      key="newListForm"
+    >
+      <div class="mb-4">
+        <label for="">New List Name</label>
+        <input
+          v-model.trim="newList.title"
+          type="text"
+          class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          autofocus
+        />
+      </div>
+      <button
+        class="border border-gray-500 rounded px-4 py-2 text-gray-500 bg-white hover:bg-gray-500 hover:text-white"
+        @click="openNewListForm = false"
+      >
+        Cancel
+      </button>
+      <button
+        class="border border-white-500 rounded px-4 py-2 text-blue-500 bg-white hover:bg-blue-500 hover:text-white"
+        @click="tryCreateList"
+      >
+        Submit
       </button>
     </div>
     <ul class="list-none">
@@ -32,7 +60,11 @@
 export default {
   data() {
     return {
-      lists: []
+      lists: [],
+      openNewListForm: false,
+      newList: {
+        title: ''
+      }
     }
   },
   mounted() {
@@ -44,6 +76,30 @@ export default {
       .catch(e => {
         console.error(e)
       })
+  },
+  methods: {
+    tryCreateList() {
+      if (this.newList.title.trim() === 0) {
+        return
+      }
+      this.openNewListForm = false
+      this.$repo.lists
+        .create({
+          title: this.newList.title
+        })
+        .then(response => {
+          this.lists.push(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
   }
 }
 </script>
+
+<style lang="postcss">
+#sidebar .nuxt-link-exact-active {
+  @apply bg-blue-600 text-white no-underline;
+}
+</style>
