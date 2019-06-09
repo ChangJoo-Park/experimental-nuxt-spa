8aa270e065350b4acf6e9a2e60b8a8f6f7e00776
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-09 15:02:26 +0200

    모달 백그라운드 컬러 수정

--- components/Common/Modal.vue
+++ components/Common/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="backdrop fixed w-screen h-screen top-0 left-0 flex flex-col items-center justify-center"
-    style="background-color: rgba(0, 0, 0, 0.1);"
+    style="background-color: rgba(0, 0, 0, 0.3);"
   >
     <div
       class="flex flex-col items-center justify-center w-3/5 min-w-md bg-white border-t-8 border-red-400 rounded-t"
