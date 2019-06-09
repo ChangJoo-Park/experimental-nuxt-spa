94172812967ec53f4bcf7a3922ea228e3eb93efd
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-09 19:56:54 +0200

    모달 스타일 수정

--- components/Common/Modal.vue
+++ components/Common/Modal.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="backdrop fixed w-screen h-screen top-0 left-0 flex flex-col items-center justify-center"
+    class="backdrop fixed w-screen h-screen top-0 left-0 flex flex-col items-center justify-center z-50"
     style="background-color: rgba(0, 0, 0, 0.3);"
   >
     <div
-      class="flex flex-col items-center justify-center w-3/5 min-w-md bg-white border-t-8 border-red-400 rounded-t"
+      class="flex flex-col items-center justify-center w-3/5 min-w-md bg-white border-t-8 border-red-400 rounded"
     >
       <slot name="header" />
       <slot name="body" />
