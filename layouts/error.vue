5d575f4ccd54cd4dfb0a06eb83c68eaebd99094b
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-11 22:24:28 +0200

    커스텀 에러 추가

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container h-full flex flex-col justify-center items-center">
+    <div v-if="error.statusCode === 404" class="w-full text-center">
+      <img
+        src="~/assets/images/404_page_not_found_2.svg"
+        class="inline-block mb-4 select-none w-4/5"
+      />
+    </div>
+    <h1 v-else>An error occurred</h1>
+    <div>
+      <nuxt-link to="/">Home page</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
